5f45223afaf0f94143c213ce08e9272fe61acc94
cQula <doktorsad@yandex.ru>
2022-11-12 14:01:37 +0300

    Intial commit

---
+++ cpp1/cpp1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+
+int *creat_array(std::fstream& in, int &size) {
+    in >> size;
+    int *array = new int[size];
+    for (int i = 0; i < size; i++) {
+        in >> array[i];
+    }
+    return array;
+}
+
+
+int main()
+{
+    int size_n = 0, size_m = 0;
+    std::fstream in("in.txt");
+    if (!in.is_open()) {
+        std::cout << "fail <in.txt> is not open";
+    }
+    int *n = creat_array(in, size_n);
+    int *m = creat_array(in, size_m);
+    in.close();
+    std::fstream out;
+    out.open("out.txt");
+    if (!out.is_open()) {
+        std::cout << "fail <out.txt> is not open";
+    }
+    out << size_m << '\n' << m[size_m - 1] << ' ';
+    for (int i = 0 ; i < size_m -1; i++){
+        out << m[i] << ' ';
+    }
+    out << '\n' << size_n << '\n';
+    for (int i = 1; i < size_n; i++) {
+        out << n[i] << ' ';
+    }
+    out << n[0];
+    out.close();
+    delete[] n, m;
+    return 0;
+}
+
